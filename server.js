f54996d0410192e5daa42558a103520bab31f3e9
user.name : "Yagyesh <user.email : "yagyeshyadav2005@gmail.com>
2025-02-05 22:25:44 +0530

    added routes

--- server.js
+++ server.js
@@ -2,6 +2,35 @@ const express = require("express");
 const app= express()
 const mongoose = require("mongoose")
 
-app.post("/login" , (req,res)=>{
+app.use(express.json())
+
+app.post("/user/signup" , (req,res)=>{
+    res.json({
+        message 
+    })
+
+})
+app.post("/user/login" , (req,res)=>{
+    
+
+
+})
+
+app.get("/user/purchases" , (req,res)=>{
+    
+
+
+})
+
+app.post("/course/purchase" , (req,res)=>{
+
+})
+
+app.get("/courses" , (req,res)=>{
     
 })
+
+
+
+
+app.listen(3000);
